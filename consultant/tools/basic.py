f3630668bb756a8fb5d372b64c0cdce3dd24f17e
HugoTheDeveloper <telioko@tutantota.com>
2024-12-13 11:01:31 +0000

    add exporters to GP

--- consultant/tools/basic.py
+++ consultant/tools/basic.py
@@ -11,3 +11,20 @@ def from_xlsx(session, path, **kwargs):
     path = normalize_path(path)
     df_pandas = pd.read_excel(path, **kwargs)
     return session.createDataFrame(df_pandas)
+
+
+def get_jdbc_url_for_gp():
+    GP_HOST = os.getenv('GP_HOST')
+    GP_PORT = os.getenv('GP_PORT')
+    GP_DB_NAME = os.getenv('GP_DB_NAME')
+    return f"jdbc:postgresql://{GP_HOST}:{GP_PORT}/{GP_DB_NAME}"
+
+
+def get_properies_for_gp():
+    DB_USER = os.getenv('DB_USER')
+    DB_PASS = os.getenv('DB_PASS')
+    return {
+        "user": DB_USER, 
+        "password": DB_PASS,
+        "driver": "org.postgresql.Driver"
+    }
